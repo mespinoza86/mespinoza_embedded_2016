91b5587103934d8f3a36e6f6e4aff342001dead0
Marco Espinoza <mespinoza1986@gmail.com>
2016-10-01 16:16:06 -0600

    Application working without autotools format

--- assignment_1/part2_LD_PRELOAD/libmemcheck.c
+++ assignment_1/part2_LD_PRELOAD/libmemcheck.c
@@ -4,35 +4,57 @@
 #include <dlfcn.h>
 
 
+int free_count=0; 
+int malloc_count=0;
+
+
 static void* (*real_malloc)(size_t)=NULL;
 
 static void mtrace_init(void)
 {
-    real_malloc = dlsym(RTLD_NEXT, "malloc");
-    if (NULL == real_malloc) {
-        fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
-    }
+	real_malloc = dlsym(RTLD_NEXT, "malloc");
+	if (NULL == real_malloc) {
+		fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
+	}
 }
 
 
+void mem_leaks_analysis (){
+	int mem_free; 
+	int mem_leaks;
+
+	mem_free = free_count;
+	mem_leaks = malloc_count - free_count;
+
+	fprintf(stderr,"Analysis finished!\n");	
+	fprintf(stderr,"Memory allocations: %i\n", malloc_count);
+	fprintf(stderr,"Memory free: %i\n", free_count);
+	fprintf(stderr,"Total memory leaks found %i\n", mem_leaks);
+
+}
+
 void *malloc(size_t size)
 {
-    if(real_malloc==NULL) {
-        mtrace_init();
-    }
+	if(real_malloc==NULL) {
+		mtrace_init();
+	}
 
-    void *p = NULL;
-    fprintf(stderr, "malloc(%d) = ", size);
-    p = real_malloc(size);
-    fprintf(stderr, "%p\n", p);
-    return p;
+	void *p = NULL;
+	fprintf(stderr, "malloc(%d) = ", size);
+	p = real_malloc(size);
+	fprintf(stderr, "%p\n", p);
+	malloc_count++;
+	return p;
 }
 
 
 void free(void *p)
 {
-   void (*libc_free)(void*) = dlsym(RTLD_NEXT, "free");
-    fprintf(stderr,"free %p\n", p);
-    libc_free(p);
+	void (*libc_free)(void*) = dlsym(RTLD_NEXT, "free");
+	fprintf(stderr,"free %p\n", p);
+	libc_free(p);
+	free_count++;
+	mem_leaks_analysis();
 }
+
 
