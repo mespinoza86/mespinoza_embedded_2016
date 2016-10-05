17ab564c10b91a0d794bc8064344ef70cb6de540
Marco Espinoza <mespinoza1986@gmail.com>
2016-10-04 21:59:06 -0600

    Version almost done

--- assignment_1/memcheck/lib/libmemcheck.c
+++ assignment_1/memcheck/lib/libmemcheck.c
@@ -1,9 +1,15 @@
 #define _GNU_SOURCE
 
+
 #include <stdio.h>
 #include <dlfcn.h>
-
-
+#include "config.h"
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h> 
+#include <errno.h>  
+#include <fcntl.h> 
+#include <unistd.h>
 int free_count=0; 
 int malloc_count=0;
 
@@ -19,20 +25,8 @@ static void mtrace_init(void)
 }
 
 
-void mem_leaks_analysis (){
-	int mem_free; 
-	int mem_leaks;
 
-	mem_free = free_count;
-	mem_leaks = malloc_count - free_count;
-
-	fprintf(stderr,"Analysis finished!\n");	
-	fprintf(stderr,"Memory allocations: %i\n", malloc_count);
-	fprintf(stderr,"Memory free: %i\n", free_count);
-	fprintf(stderr,"Total memory leaks found %i\n", mem_leaks);
-
-}
-
+#ifdef HAVE_MALLOC
 void *malloc(size_t size)
 {
 	if(real_malloc==NULL) {
@@ -40,21 +34,45 @@ void *malloc(size_t size)
 	}
 
 	void *p = NULL;
-	fprintf(stderr, "malloc(%d) = ", size);
+//	fprintf(stderr, "malloc(%d) = ", size);
 	p = real_malloc(size);
-	fprintf(stderr, "%p\n", p);
+//	fprintf(stderr, "%p\n", p);
 	malloc_count++;
 	return p;
 }
+#endif
+
+
+#ifdef HAVE_MEMORY_H 
+
+void mem_leaks_analysis (){
+	int mem_free; 
+	int mem_leaks;
+
+	mem_free = free_count;
+	mem_leaks = malloc_count - free_count;
+
+
+	fprintf(stderr,"Analysis finished!\n");	
+	fprintf(stderr,"Memory allocations: %i\n", malloc_count);
+	fprintf(stderr,"Memory free: %i\n", free_count);
+	fprintf(stderr,"Total memory leaks found %i\n", mem_leaks);
+
+
+}
+
 
 
 void free(void *p)
 {
 	void (*libc_free)(void*) = dlsym(RTLD_NEXT, "free");
-	fprintf(stderr,"free %p\n", p);
+//	fprintf(stderr,"free %p\n", p);
 	libc_free(p);
 	free_count++;
-	mem_leaks_analysis();
 }
 
+#endif
 
+__attribute__((destructor)) void end (void){
+	mem_leaks_analysis();
+}
